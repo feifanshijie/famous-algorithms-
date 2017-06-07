9d74712b1e67cc0b935075ab5539d0fc7c79b755
gym <2652525544@qq.com>
2017-06-08 00:02:42 +0800

    。

--- Heap_Sort.php
+++ Heap_Sort.php
@@ -2,11 +2,26 @@
 /**
  * Name: 二分法查找
  */
-$array = [5,1,4,6,2,10,3,7,8,9];
+$array = [1,2,3,5,6,7,9,11,12,15];
 
-function run()
+function run($key, $array)
 {
-
+    $k = ceil(count($array)/2);
+    $low = 0;
+    $high = count($array)-1;
+    while ($low <= $high)
+    {
+        if($array[$k] == $key)
+        {
+            return true;
+        }
+        if($array[$k] > $key)
+        {
+            $low = $k + 1;
+        } else {
+            $high = $k - 1;
+        }
+    }
+    return false;
 }
-
 
